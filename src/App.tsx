2b5730a0620d2fa18a652b925832eb8476a8abb9
blacknii <42908977+blacknii@users.noreply.github.com>
2023-06-23 15:10:56 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,57 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-import PokemonList from "./PokemonList";
+// import axios from "axios";
+import PokemonList from "./components/PokemonList";
 import "./App.css";
+import { usePokemonData } from "./hooks/usePokemonData";
 
-type PokemonData = {
-  count: number;
-  next: string | null;
-  previous: string | null;
-  results: { name: string; url: string }[];
-};
+// type PokemonData = {
+//   count: number;
+//   next: string | null;
+//   previous: string | null;
+//   results: { name: string; url: string }[];
+// };
 
-type PokemonDetails = {
-  name: string;
-  image: string;
-  types: string[];
-};
+// type PokemonDetails = {
+//   name: string;
+//   image: string;
+//   types: string[];
+// };
 
-const fetchAllPokemon = async (url: string) => {
-  const response = await axios.get<PokemonData>(url);
-  const pokemonData = response.data;
+// const fetchAllPokemon = async (url: string) => {
+//   const response = await axios.get<PokemonData>(url);
+//   const pokemonData = response.data;
 
-  const promises = pokemonData.results.map((p) =>
-    axios.get(p.url).then((res) => ({
-      name: p.name,
-      image: res.data.sprites.front_default,
-      types: res.data.types.map((t: any) => t.type.name),
-    }))
-  );
+//   const promises = pokemonData.results.map((p) =>
+//     axios.get(p.url).then((res) => ({
+//       name: p.name,
+//       image: res.data.sprites.front_default,
+//       types: res.data.types.map((t: any) => t.type.name),
+//     }))
+//   );
 
-  const pokemon = await Promise.all(promises);
+//   const pokemon = await Promise.all(promises);
 
-  if (pokemonData.next) {
-    const nextPokemon = await fetchAllPokemon(pokemonData.next);
-    return pokemon.concat(nextPokemon);
-  } else {
-    return pokemon;
-  }
-};
+//   if (pokemonData.next) {
+//     const nextPokemon = await fetchAllPokemon(pokemonData.next);
+//     return pokemon.concat(nextPokemon);
+//   } else {
+//     return pokemon;
+//   }
+// };
 
 const App: React.FC = () => {
   const [pokemon, setPokemon] = useState<PokemonDetails[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    setLoading(true);
-    fetchAllPokemon("https://pokeapi.co/api/v2/pokemon").then((pokemon) => {
-      setPokemon(pokemon);
-      setLoading(false);
-    });
-  }, []);
+  usePokemonData(1);
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   fetchAllPokemon("https://pokeapi.co/api/v2/pokemon").then((pokemon) => {
+  //     setPokemon(pokemon);
+  //     setLoading(false);
+  //   });
+  // }, []);
 
   if (loading) return <p>Loading...</p>;
 
