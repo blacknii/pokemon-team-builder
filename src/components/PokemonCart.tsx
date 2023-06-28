2c755cdf149e783a37ca3ec13fe0664792241609
blacknii <42908977+blacknii@users.noreply.github.com>
2023-06-28 18:52:24 +0200

    .

--- src/components/PokemonCart.tsx
+++ src/components/PokemonCart.tsx
@@ -1,15 +1,25 @@
 import styles from "./PokemonCart.module.css";
+import usePokemonDetails from "../hooks/usePokemonDetails";
+import { useState } from "react";
 
 interface PokemonCartProps {
   pokemon: string[];
 }
 
 const PokemonCart: React.FC<PokemonCartProps> = ({ name, image }) => {
+  const [isActive, setisActive] = useState(false);
+  const pokemonDetails = usePokemonDetails(isActive, name, image);
+
+  const clickHandler = () => {
+    setisActive(true);
+    console.log(pokemonDetails);
+  };
+
   return (
     <div
       className={styles["container"]}
       onClick={() => {
-        console.log(name);
+        clickHandler(name);
       }}
     >
       <p>{name}</p>
